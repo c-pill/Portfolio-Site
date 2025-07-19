276f2b477f3edd6cd46a5d29bc423359c744df53
c-pill <81812824+c-pill@users.noreply.github.com>
2025-07-19 18:38:50 -0400

    Portfolio Site v1.1.0

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ import React from "react";
 export default function Document() {
   return (
     <Html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <Head/>
       <body>
         <Main />
